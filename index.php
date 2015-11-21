1e0dad7844f68c9f8f2381457c30b5e883b0eb90
David Petringa <david@ajaxtransport.com>
2015-11-21 14:38:49 -0600

    Revert "Added Constant SITE_NAME"
    This reverts commit ad93f2b68bcfb062f8de2e6e89bf53f513236227.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if($_SERVER['REQUEST_METHOD'] ==='POST')
 	{
 		include('./includes/login.inc.php');
 	}
-$page_title ="Home | ".SITE_NAME."";
+$page_title ="Home | Scrapinventory";
 include('./views/header.inc.html');
 include('./views/index.inc.html');
 include('./views/footer.inc.html');
