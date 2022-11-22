50f32892725f966c2d24ca7a6c9b173e964c5995
David Petringa <david@davidpetringa.com>
2022-11-22 11:19:44 -0600

    fix error

--- forgot_password.php
+++ forgot_password.php
@@ -102,8 +102,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST')
 				'body'    => '<span style="color: red">Scrapinventory Registered</span><br> '.$body. '',
 				'altBody' => $altBody
 			);
-
-			sendMail($data);
+print_r($data);
+			//sendMail($data);
 
 			/**************************************end use phpmailer and mailgun********************************/
 
