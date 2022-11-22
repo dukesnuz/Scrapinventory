b4c39950f025a9ba2f762cb802c4b478b2a1d6dd
David Petringa <david@davidpetringa.com>
2022-11-22 15:56:32 -0600

    fix error

--- includes/send_mail.php
+++ includes/send_mail.php
@@ -3,8 +3,9 @@
 *****function to take arguments and then send an email using mailgun and phh mailer****************
 ***************************************************************************************************/
 function sendMail($data){
-  //print_r($data);
+  print_r($data);
   require_once ('../vendor/autoload.php');
+  echo 3;
   $mail = new PHPMailer;
   $mail->isSMTP();
   $mail->Host = MAILGUN_HOST;
